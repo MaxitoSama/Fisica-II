11e3819cba67e3c287bc9c4f81fc74c34001d810
Max <marc.garcia.u@gmail.com>
2017-10-25 10:22:56 +0200

    3D

---
+++ Physics3D_class1 - handout/ModuleSceneIntro.cpp
@@ -0,0 +1,88 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	// TODO 2: Place the camera one unit up in Y and one unit to the right
+	// experiment with different camera placements, then use LookAt()
+	// to make it look at the center
+	App->camera->Move({ 0,1,0 });
+	App->camera->LookAt({ 0,0,0 });
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleSceneIntro::Update()
+{
+	// TODO 1: Create a Plane primitive. This uses the plane formula
+	// so you have to express the normal of the plane to create 
+	// a plane centered around 0,0. Make that it draw the axis for reference
+
+	// TODO 6: Draw a sphere of 0.5f radius around the center
+	// Draw somewhere else a cube and a cylinder in wireframe
+
+	Color colore(255.0f, 0.0f, 100.0f, 0.0f);
+
+	Plane first_plane(0,1,0,0);
+	first_plane.axis=true;
+	first_plane.Render();
+
+	Sphere first_sphere(0.5f);
+	first_sphere.SetPos(0, 0, 0);
+	first_sphere.color = colore;
+	first_sphere.Render();
+
+	Sphere second_sphere(0.5f);
+	second_sphere.SetPos(1.0f, 0, 0);
+	second_sphere.color = colore;
+	second_sphere.wire = true;
+	second_sphere.Render();
+
+	Sphere third_sphere(0.52f);
+	third_sphere.SetPos(0.5f, 5, 0);
+	third_sphere.color = colore;
+	third_sphere.wire = true;
+	third_sphere.Render();
+
+	Cylinder first_cylinder(0.5f, 5);
+	first_cylinder.SetPos(0.5f, 2.5f, 0);
+	first_cylinder.SetRotation(90, { 0,0,1 });
+	first_cylinder.color=colore;
+	first_cylinder.wire = true;
+	first_cylinder.Render();
+
+	MouseRotation();
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::MouseRotation()
+{
+	int x;
+	x=App->input->GetMouseXMotion();
+	LOG("%d", x);
+
+
+}
