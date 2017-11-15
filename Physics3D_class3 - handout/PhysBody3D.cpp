6f7ffba054ef72bc57254ddef0b7907d6f802f5f
Max <marc.garcia.u@gmail.com>
2017-11-15 10:30:03 +0100

    new handout

---
+++ Physics3D_class3 - handout/PhysBody3D.cpp
@@ -0,0 +1,34 @@
+#include "Application.h"
+#include "PhysBody3D.h"
+#include "ModulePhysics3D.h"
+#include "glmath.h"
+#include "Bullet/include/btBulletDynamicsCommon.h"
+
+// TODO 4: Fill in PhysBody methods. Remember to use 
+// get/setOpenGLMatrix methods
+
+class Application;
+
+// ---------------------------------------------------------
+PhysBody3D::PhysBody3D(btRigidBody* body)
+{}
+
+// ---------------------------------------------------------
+PhysBody3D::~PhysBody3D()
+{}
+
+// ---------------------------------------------------------
+void PhysBody3D::GetTransform(float* matrix) const
+{
+	
+}
+
+// ---------------------------------------------------------
+void PhysBody3D::SetTransform(const float* matrix) const
+{
+}
+
+// ---------------------------------------------------------
+void PhysBody3D::SetPos(float x, float y, float z)
+{
+}
